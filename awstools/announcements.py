94663ec8ae39fb50d21bcab9939314d779998adb
dvdg6566 <czhdaniel@gmail.com>
2023-01-31 19:15:25 +0800

    Connected announcements to websocket

--- awstools/announcements.py
+++ awstools/announcements.py
@@ -5,6 +5,7 @@ from uuid import uuid4
 from datetime import datetime
 
 from awstools import awshelper
+from awstools import websocket
 from boto3.dynamodb.conditions import Key
 
 dynamodb = boto3.resource('dynamodb')
@@ -23,5 +24,6 @@ def createAnnouncement(title, text):
 	info['announcementTime'] = str(datetime.utcnow().strftime("%Y-%m-%d %X"))
 
 	# Trigger AWS Lambda function to activate announcement notification
+	websocket.announce()
 
 	announcements_table.put_item(Item=info)
--- awstools/websocket.py
+++ awstools/websocket.py
@@ -1,1 +1,29 @@
 # Invokes lambda functions to ping websockets
+# If performance degrades, can consider using EC2 side to trigger Step Function
+
+import os
+import json
+import boto3
+lambda_client = boto3.client('lambda')
+
+judgeName = os.environ.get('JUDGE_NAME')
+accountId = os.environ.get('AWS_ACCOUNT_ID')
+region = os.environ.get('AWS_REGION')
+
+WEBSOCKET_INVOKE_LAMBDA_NAME = f'arn:aws:lambda:{region}:{accountId}:function:{judgeName}-websocket-invoke'
+
+def announce():
+	# Ping all users
+	lambda_input = {'notificationType': 'announce'}
+	res = lambda_client.invoke(FunctionName = WEBSOCKET_INVOKE_LAMBDA_NAME, InvocationType='Event', Payload = json.dumps(lambda_input))
+	print(res)
+
+def postClarification():
+	# Ping all admins
+	lambda_input = {'notificationType': 'postclarification'}
+	res = lambda_client.invoke(FunctionName = WEBSOCKET_INVOKE_LAMBDA_NAME, InvocationType='Event', Payload = json.dumps(lambda_input))
+
+def answerClarification(username):
+	# Ping any connection for specified username
+	lambda_input = {'notificationType': 'answerclarification', 'username': username}
+	res = lambda_client.invoke(FunctionName = WEBSOCKET_INVOKE_LAMBDA_NAME, InvocationType='Event', Payload = json.dumps(lambda_input))
