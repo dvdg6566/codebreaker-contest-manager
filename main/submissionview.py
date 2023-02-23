549e6182d355beaee90dea836564b4e50c0beb9b
dvdg6566 <czhdaniel@gmail.com>
2023-02-23 15:54:10 +0800

    Updated grading to be in separate file in awstools

--- main/submissionview.py
+++ main/submissionview.py
@@ -159,7 +159,7 @@ def submission(subId):
 				return redirect(f'/problem/{problemName}')
 
 			# For regrade, the code is already there so we just call grade 
-			awstools.submissions.gradeSubmission(
+			awstools.submissions.startGrading(
 				problemName = problemName,
 				submissionId = subId,
 				username = subDetails['username'], 
@@ -180,43 +180,20 @@ def submission(subId):
 			flash("Sorry, this problem is still incomplete.", "warning")
 			return redirect(f'/problem/{problemName}')
 
-		result = request.form 
-		code, codeA, codeB = '','',''
-		if problemInfo['problem_type'] == 'Communication':
-			codeA = result['codeA']
-			codeB = result['codeB']
-		else:
-			code = result['code']
-
-		if max(len(code), len(codeA), len(codeB)) > 128000:
-			flash("Sorry, your code is too long.", "warning")
-			return redirect(f'/problem/{problemName}')
-
-		# Assign new submission index
-		newSubId = awstools.submissions.getNextSubmissionId()
-
-		if problemInfo['problem_type'] == 'Communication':
-			# Upload code file to S3
-			s3pathA = f'source/{newSubId}A.{language}'
-			s3pathB = f'source/{newSubId}B.{language}'
-			awstools.submissions.uploadSubmission(code = codeA, s3path = s3pathA)
-			awstools.submissions.uploadSubmission(code = codeB, s3path = s3pathB)
-		else:
-			# Upload code file to S3
-			s3path = f'source/{newSubId}.{language}'
-			awstools.submissions.uploadSubmission(code = code, s3path = s3path)
-
-		awstools.submissions.gradeSubmission(
-			problemName = problemName,
-			submissionId = newSubId,
-			username = subDetails['username'], 
-			submissionTime = None,
-			language = language,
-			problemType = problemInfo['problem_type']
+		''' GRADING '''
+		response = awstools.grading.gradeSubmission(
+			form = request.form,
+			problemInfo = problemInfo,
+			userInfo = userInfo,
+			language = language
 		)
 
+		if response['status'] != 200:
+			flash(response['error'], "warning")
+			return redirect(f'/problem/{problemName}')
+
 		time.sleep(2)
-		return redirect(f"/submission/{newSubId}")
+		return redirect(f"/submission/{response['subId']}")
 
 	'''END RESUBMISSION'''
 
